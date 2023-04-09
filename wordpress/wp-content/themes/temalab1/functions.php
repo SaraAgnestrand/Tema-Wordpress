d49c8e948c5972a8597c2f7f546fddf18cef0003
Sara Agnestrand <sara@agnestrand.net>
2023-04-09 14:54:36 +0200

    added changes for styles in functions.php

--- wordpress/wp-content/themes/temalab1/functions.php
+++ wordpress/wp-content/themes/temalab1/functions.php
@@ -1,14 +1,14 @@
 <?php
 
-function add_theme_styles(){
-    wp_enqueue_style('style', get_template_directory_uri(). "/style.css", array(), '1.0', 'all');
-    wp_enqueue_style('style-bootstrap', get_template_directory_uri(). "css/bootstrap.css", array(), '1.0', 'all');
-    wp_enqueue_style('style-fontawesome', get_template_directory_uri(). "css/font-awesome.css", array(), '1.0', 'all');
-
+function add_theme_scripts(){
+    wp_enqueue_style('bootstrap-style', get_template_directory_uri().'assets/css/bootstrap.css', array(), 1.0, 'all');
+    wp_enqueue_style('font-style', get_template_directory_uri().'assets/css/font-awsome.css', array(), 1.0, 'all');
+    wp_enqueue_style('style', get_template_directory_uri().'assets/css/style.css', array(), 1.0, 'all');
+    
 }
 
-add_action( 'wp_enqueue_scipts', 'style');
-add_action( 'wp_enqueue_scipts', 'style-bootstrap');
-add_action( 'wp_enqueue_scipts', 'style-fontawesome');
+add_action('wp_scripts', 'add_theme_scripts)';
+
+
 
 ?>
